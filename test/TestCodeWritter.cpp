26b7a87f75e2c575cd9137c9cca31c87f7466b10
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2025-08-28 22:48:17 +0200

    Feat: Doing a lot of modifications, for better development

---
+++ test/TestCodeWritter.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "CodeWritter.hpp"
+// string pushAssemblyTemplate=""
+// string popAssemblyTemplate=""
+// string arithmeticAssemblyTemplate=
+// string writeLabelTemplate=
+// string writeGotoTemplate=
+// string writeIfTemplate=
+
+
+TEST(TestCodeWritter,HandleGetPushAssembly){
+
+}
+TEST(TestCodeWritter, HandleGetPopAssembly){}
+TEST(TestCodeWritter, HandleGetArithmeticAssembly){}
+TEST(TestCodeWritter, HandleGetWriteLabelTemplate){}
+TEST(TestCodeWritter, HandleGetWriteGotoTemplate){}
+TEST(TestCodeWritter, HandleGetWriteIfTemplate){}
---
+++ test/TestCodeWritter.hpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+using testing::Test;
+class TestCodeWritter : public Test {
+  // Sets up the test fixture.
+
+ protected:
+  void SetUp() override;
+  // Tears down the test fixture.
+  void TearDown() override;
+}
