5433fcd835686899bfeaa02f9a7546ff21a8e0a4
Juan Pablo Prada Mejia <juanpabloprada02112000@gmail.com>
2025-10-02 22:59:10 +0200

    Feat: Adding file and directory handling

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,38 +1,47 @@
 #include "CodeWritter.hpp"
 #include "TestParser.hpp"
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-      cout << "Remember passing second argument, the file vm file to be "
-              "translater "
-           << endl;
-      return EXIT_FAILURE;
-  }
-  string filename = argv[1];
-  Control c       = Control (filename);
-  c.traverseFile ();
-  // t' = push constant 3
-  // t''' = push constant -3
-  // t''' = push arg 3
-  // CodeWritter cw = CodeWritter();
+int main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		cout << "Remember passing second argument, the file vm file to be "
+			"translater "
+		     << endl;
+		return EXIT_FAILURE;
+	}
+	string filepath = argv[1];
+	Control c = Control(filepath);
+	c.start_control(filepath);
 
-  // std::cout << cw.getArithmeticAssembly("not") << std::endl;
-  // std::cout << cw.getArithmeticAssembly("neg") << std::endl;
-  // std::cout << cw.getArithmeticAssembly("lt") << std::endl;
-  //
-  // std::cout << cw.getWriteIfTemplate("LOOP")<<std::endl;
-  // std::cout << cw.getWriteGotoTemplate("LOOP")<<std::endl;
+	// c.traverseFile ();
+	// t' = push constant 3
+	// t''' = push constant -3
+	// t''' = push arg 3
+	// Parser p = Parser();
+	// p.operateCommand("call function 3");
+	// if(true){
+	// 	{}
+	// }
 
-  // std::cout << "RUNNING EQ:"<< std::endl;
-  // std::cout << cw.getArithmeticAssembly("eq") << std::endl;
-  // std::cout << "RUNNING GT:"<< std::endl;
-  // std::cout << cw.getArithmeticAssembly("gt") << std::endl;
-  // std::cout << "RUNNING LT:"<< std::endl;
-  // std::cout << cw.getArithmeticAssembly("lt") << std::endl;
-  // std::cout <<;
+	// CodeWritter cw = CodeWritter();
 
-  // std::cout << "CASE IS POINTER | TEMP | STATIC" << std::endl;
-  // std::cout << cw.getPopAssembly(5, 6 + NOTREFMS, 13, 14) << std::endl;
-  // std::cout << "CASE IS NOT POINTER | TEMP | STATIC" << std::endl;
-  // std::cout << cw.getPopAssembly(0, 6, 13, 14) << std::endl;
-  // cout << cw.getPushAssembly("argument", 2) << endl;
+	// std::cout << cw.getArithmeticAssembly("not") << std::endl;
+	// std::cout << cw.getArithmeticAssembly("neg") << std::endl;
+	// std::cout << cw.getArithmeticAssembly("lt") << std::endl;
+	//
+	// std::cout << cw.getWriteIfTemplate("LOOP")<<std::endl;
+	// std::cout << cw.getWriteGotoTemplate("LOOP")<<std::endl;
+
+	// std::cout << "RUNNING EQ:"<< std::endl;
+	// std::cout << cw.getArithmeticAssembly("eq") << std::endl;
+	// std::cout << "RUNNING GT:"<< std::endl;
+	// std::cout << cw.getArithmeticAssembly("gt") << std::endl;
+	// std::cout << "RUNNING LT:"<< std::endl;
+	// std::cout << cw.getArithmeticAssembly("lt") << std::endl;
+	// std::cout <<;
+
+	// std::cout << "CASE IS POINTER | TEMP | STATIC" << std::endl;
+	// std::cout << cw.getPopAssembly(5, 6 + NOTREFMS, 13, 14) << std::endl;
+	// std::cout << "CASE IS NOT POINTER | TEMP | STATIC" << std::endl;
+	// std::cout << cw.getPopAssembly(0, 6, 13, 14) << std::endl;
+	// cout << cw.getPushAssembly("argument", 2) << endl;
 }
